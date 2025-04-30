2b1bb67d0c0742cfa54cbef7da00eabdbce519ef
Nodir <noki1330@gmail.com>
2025-04-30 13:37:20 +0500

    first commit

---
+++ callback.php
@@ -0,0 +1,35 @@
+<?php
+// callback.php
+
+if (!isset($_GET['code'])) {
+	die('Authorization code not found');
+}
+
+$code = $_GET['code'];
+
+$client_id = 'f4ef52c6-9432-49ae-bfc0-9dcb399ec15b';
+$client_secret = 'K1d8zhwTUaeljYVDA6dKrO9AFbZWX8yVdIbqdctWtklYyHmmfJRs9IgKD8UrVv0N';
+$redirect_uri = 'http://nodir.fwh.is/callback.php';
+$subdomain = 'khatamovnodir1301';
+
+$url = "https://{$subdomain}.amocrm.ru/oauth2/access_token";
+
+$data = [
+	'grant_type' => 'authorization_code',
+	'client_id' => $client_id,
+	'client_secret' => $client_secret,
+	'code' => $code,
+	'redirect_uri' => $redirect_uri
+];
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+file_put_contents('token.json', $response);
+echo "Token saqlandi:<br><pre>$response</pre>";
---
+++ webhook.php
@@ -0,0 +1,55 @@
+<?php
+// webhook.php
+
+$input = json_decode(file_get_contents('php://input'), true);
+$tokenData = json_decode(file_get_contents('token.json'), true);
+
+$access_token = $tokenData['access_token'];
+$subdomain = 'YOUR_SUBDOMAIN';
+
+$event = $input['event'];
+$object = $input['object'];
+$type = $object['type'];
+$id = $object['id'];
+
+if (!$type || !$id) {
+	http_response_code(400);
+	exit("No valid data");
+}
+
+$noteText = '';
+if ($event === 'add') {
+	$noteText = "Yangi $type qo‘shildi\nID: $id\nSana: " . date('Y-m-d H:i:s');
+} elseif ($event === 'update') {
+	$noteText = "$type yangilandi\nID: $id\nSana: " . date('Y-m-d H:i:s');
+} else {
+	exit('Unsupported event');
+}
+
+$entity = $type === 'contact' ? 'contacts' : 'leads';
+$noteData = [
+	[
+		'entity_id' => $id,
+		'note_type' => 'common',
+		'params' => ['text' => $noteText]
+	]
+];
+
+$url = "https://{$subdomain}.amocrm.ru/api/v4/{$entity}/notes";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+	"Authorization: Bearer $access_token",
+	"Content-Type: application/json"
+]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($noteData));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+file_put_contents('log.txt', "[" . date('Y-m-d H:i:s') . "]\n$response\n\n", FILE_APPEND);
+http_response_code(200);
+echo "✅ Webhook ishladi: $event $type ID: $id\n";
